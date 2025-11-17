b311c409c6a836c2cf3b45a04d70fd61b4842003
Hamza Shahbaz <170954528+hamzaraj9121@users.noreply.github.com>
2025-11-17 18:46:49 +0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 // next.config.mjs
-/** @type {import('next').NextConfig} */
+//  @type {import('next').NextConfig} 
 const nextConfig = {
+   compiler: {
+    removeConsole: false,
+     swcMinify: true,
+  },
   experimental: {
     optimizePackageImports: ['react-icons'], // disables just for react-icons
   },
 };
+
+
 
 export default nextConfig;
